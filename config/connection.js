eb5eb28a965fe70ca0fa58d22ab1e1cf0b6370c9
loringmuller <loringmuller@gmail.com>
2019-03-26 20:40:39 -0700

    edits

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Password",
-  database: "burgers_db"
-});
+var connection;
 
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -19,6 +13,16 @@ if (process.env.JAWSDB_URL){
     database: 'burgers_db'
   });
 };
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Password",
+  database: "burgers_db"
+});
+
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
